30cb4aea396fd31e340908e2c06d90b10f28a1bf
Anthonynoel22 <anthony.noel-kiruparajah@oclock.school>
2026-01-07 15:19:04 +0100

    WIP all components

--- portfolio/my-react-app/src/App.jsx
+++ portfolio/my-react-app/src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-// import Header from "./components/Header/Header.jsx;"
+import Header from "./components/Header/Header.jsx";
 import Home from "./components/Home/Home.jsx";
+import Project from "./components/Project/Project.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import './App.css'
 
@@ -8,9 +11,17 @@ function App() {
 
   return (
     <Router>
+      <div className="App">
+      <Header/>
+      <main>
       <Routes>
         <Route path= "/" element={<Home/>} />
+        <Route path="/projets" element={<Project/>} />
+        <Route path="/contact" element={<Contact/>} />
       </Routes>
+      </main>
+      <Footer/>
+    </div>
     </Router>
   )
 }
--- portfolio/my-react-app/src/components/Home/Home.jsx
+++ portfolio/my-react-app/src/components/Home/Home.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 import "./Home.scss";
 
 
-const Home = () => {
+function Home(){
     return (
         <div className="home">
         <p>  Hey, je suis <strong>Anthony NOEL KIRUPARAJAH</strong>,<br />
         Je suis développeur Full-Stack, spécialisée en accessibilité numérique
         (A11Y). Passionnée par le développement front-end et back-end, j'aime
         concevoir des interfaces utilisateur intuitives et accessibles. Toujours
-        curieuse et avide de nouvelles connaissances, j'aime explorer les
+        curieux et avide de nouvelles connaissances, j'aime explorer les
         nouvelles technologies.</p>
         </div>
     );
