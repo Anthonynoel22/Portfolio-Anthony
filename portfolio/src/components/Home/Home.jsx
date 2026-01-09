76397cd04e50b9b47fe29b929302a9c637454d75
Anthonynoel22 <anthony.noel-kiruparajah@oclock.school>
2026-01-09 11:34:45 +0100

    WIP JSX AND SCSS TO ALL COMPONENTS

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -5,8 +5,8 @@ import "./Home.scss";
 function Home(){
     return (
         <div className="home">
-        <p>  Hey, je suis <strong>Anthony NOEL KIRUPARAJAH</strong>,<br />
-        Je suis développeur Full-Stack, spécialisée en accessibilité numérique
+        <h1>Hey, je suis Anthony NOEL KIRUPARAJAH</h1>
+        <p>Je suis développeur Full-Stack, spécialisée en accessibilité numérique
         (A11Y). Passionnée par le développement front-end et back-end, j'aime
         concevoir des interfaces utilisateur intuitives et accessibles. Toujours
         curieux et avide de nouvelles connaissances, j'aime explorer les
