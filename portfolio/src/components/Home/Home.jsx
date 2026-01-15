f16554b8a8bcd5d9c5cb859aa30ccf0c8195bb88
Anthonynoel22 <anthony.noel-kiruparajah@oclock.school>
2026-01-15 15:01:33 +0100

    update text home

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -11,24 +11,23 @@ function Home() {
                 </span>
             </h1>
             <p className="home-text">
-                Développeur Full-Stack, spécialisé en accessibilité
-                numérique (A11Y). J'accorde une importance particulière à la
-                création d'expériences web inclusives, intuitives et
-                performantes, en plaçant toujours l'utilisateur au centre de mes
-                réflexions. Sur le plan front-end, je conçois des interfaces
-                claires et accessibles, tout en veillant à la cohérence visuelle
-                et à la fluidité de l'expérience utilisateur. Côté back-end, je
-                m'assure du bon fonctionnement, de la sécurité et de la
-                scalabilité des applications que je développe. Passionné par
-                l'innovation et animé par la curiosité, je reste en veille
-                constante sur les nouvelles technologies et les meilleures
-                pratiques du web. Mon objectif : contribuer à un web plus
-                inclusif, durable et humain, en alliant performance technique et
-                accessibilité.
+                Développeur Full-Stack, spécialisé en accessibilité numérique
+                (A11Y). J’accorde une importance particulière à la création
+                d’expériences web inclusives, intuitives et performantes, en
+                plaçant toujours l’utilisateur au centre de mes réflexions. Sur
+                le plan front-end, je conçois des interfaces claires et
+                accessibles, tout en veillant à la cohérence visuelle et à la
+                fluidité de l’expérience utilisateur. Côté back-end, je m’assure
+                du bon fonctionnement, de la sécurité et de la scalabilité des
+                applications que je développe. Passionné par l’innovation et
+                animé par la curiosité, je reste en veille constante sur les
+                nouvelles technologies et les meilleures pratiques du web. Mon
+                objectif : contribuer à un web plus inclusif, durable et humain,
+                en alliant performance technique et accessibilité. Ce portfolio
+                est là pour présenter mes projets et permettre de me contacter.
             </p>
         </div>
     );
 }
 
 export default Home;
-
